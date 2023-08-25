beaa28c044aa6f75648dadc675dda55c80c239bf
AndriiMishch <anderito02@gmail.com>
2023-08-25 14:00:43 +0200

    Update mobile-menu.js

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,6 +3,7 @@
       openModalBtn: document.querySelector("[header-data-modal-open]"),
       closeModalBtn: document.querySelector("[header-data-modal-close]"),
       modal: document.querySelector("[header-data-modal]"),
+      body: document.querySelector('body'),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
@@ -10,5 +11,6 @@
   
     function toggleModal() {
       refs.modal.classList.toggle("is-hidden");
+      refs.body.classList.toggle("scroll-disabled");
     }
   })();
